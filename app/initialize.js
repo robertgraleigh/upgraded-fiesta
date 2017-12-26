32e65ab8da8cf57061ab15ea1b3ac9f837312138
Robert G. Raleigh <robert@helloworlddevs.com>
2017-12-26 12:23:57 -0800

    added foundation-sites, jquery, motionUI dependencies

---
+++ app/initialize.js
@@ -0,0 +1,8 @@
+// initialize.js
+/* jshint esversion: 6 */
+
+import 'foundation-sites';
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+});
